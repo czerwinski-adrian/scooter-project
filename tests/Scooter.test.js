e32706c83e7ceee28e4baee686aee7b026de7d52
czerwinski-adrian <adrianczerwinski3@gmail.com>
2023-02-10 15:23:58 +0000

    scooter.test first commit

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -2,14 +2,16 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
 //typeof scooter === object
-describe('scooter integrity checks', () => {
-  test('scooter has correct properties', () => {
+describe('scooter class integrity checks', () => {
+  test('scooter class has correct properties', () => {
     // edit this to be a real test!
-    expect(Scooter.station).toBe('Manchester', null)
-    expect(Scooter).toHaveProperty('user', null);
-    expect(Scooter).toHaveProperty('serial');
-    expect(Scooter).toHaveProperty('charge');
-    expect(typeof Scooter.isBroken).toEqual('boolean');
+    let scooter = new Scooter({station: 'Manchester', user: null});
+
+    expect(scooter.station).toBe('Manchester', null);
+    expect(scooter).toHaveProperty('user', null);
+    expect(scooter).toHaveProperty('serial');
+    expect(scooter).toHaveProperty('charge');
+    expect(typeof scooter.isBroken).toBe('boolean');
   }
 )
 })
@@ -17,13 +19,23 @@ describe('scooter integrity checks', () => {
 //Method tests
 describe('scooter methods', () => {
   // tests here!
-
+  let scooter = new Scooter();
   //rent method
-
+  test('rent scooter', () => {
+    expect(scooter.station).toBe(null);
+    expect(scooter.charge).toBeGraterThan(20);
+    expect(typeof scooter.isBroken).toBe(false);
+    expect(scooter.user).toBe('user');
+  })
   //dock method
-
+  test('dock scooter', () => {
+    expect(scooter.station).toBe('new station')
+    expect(scooter.user).toBe(null);
+  })
   //requestRepair method
 
   //charge method
-
+  test('charge scooter', () => {
+    expect(scooter.charge).toBe(100);
+  })
 })
