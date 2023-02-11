aef656233b831e0eecfb28acb0e477bb1f5a63b2
czerwinski-adrian <adrianczerwinski3@gmail.com>
2023-02-11 19:32:45 +0000

    user test first commit

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,29 @@
 const User = require('../src/User')
 
 // User tests here
-
 // test username
-
 // test password
-
 // test age
 
-// test login
+describe('user class integrity checks', () => {
+    test('user class has correct properties', () =>{
 
+    let user = new User({username: 'user', password: 'password', age: 24});
+    
+        expect(user.username).toHaveProperty('user');
+        expect(user.password).toHaveProperty('password');
+        expect(user.age).toHaveProperty('age');
+        expect(typeof user.loggedIn).toBe('boolean');
+    }
+    )
+})
+
+// Method tests 
+// test login
+test('login user', () =>{
+    expect(user.password).toBe('password');
+})
 // test logout
+test('logout user', () =>{
+    expect(typeof user.loggedIn).toBe(false);
+})
